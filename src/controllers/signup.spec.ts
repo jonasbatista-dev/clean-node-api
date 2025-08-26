05f4e88e71ea1e6186548541b5bb19f0eea55b0a
jonasbatista-dev <jonassilvat.i@hotmail.com>
2025-08-26 01:20:41 -0300

    test: ensure SignUpControlle returns body with error if no name provided

--- src/controllers/signup.spec.ts
+++ src/controllers/signup.spec.ts
@@ -1,16 +1,17 @@
-import { SignupController } from './signup';
+import { SignupController } from "./signup";
 
-describe('Signup Controller', () => {
-  test('Should return 400 if no name is provided', async () => {
+describe("Signup Controller", () => {
+  test("Should return 400 if no name is provided", async () => {
     const sut = new SignupController();
     const httpRequest = {
       body: {
-        email: 'any@email.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
+        email: "any@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
       },
     };
-    const httpResponde = sut.handle(httpRequest);
-    expect(httpResponde.statusCode).toBe(400);
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
   });
 });
