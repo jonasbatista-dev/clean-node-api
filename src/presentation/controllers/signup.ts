e476d9114e702a8dbe9ad7ecc0f6278bda8037ed
jonasbatista-dev <jonassilvat.i@hotmail.com>
2025-08-26 16:52:11 -0300

    refector: make SignUpController conforms to protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from "../errors/missing-param-error";
 import type { HttpRequest, HttpResponse } from "../protocols/https";
 import { badRequest } from "../helpers/http-helper";
+import type { Controller } from "../protocols/controler";
 
-export class SignupController {
+export class SignupController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       "name",
